8f9d2a34003bde4a4fff71cf8b733a1787de3c9d
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-08 14:40:53 +0100

    Finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0
+	for(let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let product = 1
+  for(let i = 0; i < args.length; i++) {
+    product *= args[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1
+  for(let i = 0; i < exponent; i++) {
+    result *= base
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+	let result = 1
+  for(let i = factorial; i > 0; i--) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
