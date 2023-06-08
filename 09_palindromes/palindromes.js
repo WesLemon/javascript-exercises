d9e2fe291356b137909e180e9d86aa0727ab8b2b
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-08 14:59:27 +0100

    Finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    str = str.replace(/[^a-z]/g, "")
+    arr = str.split("")
+    for (let i = 0; i < str.length / 2; i++) {
+        if (!(arr[i] === arr[(str.length - 1) - i])) {
+            return false
+        }
+    }
+    return true
 
+    // More elegant solution
+    // const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    // return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
