47124304e9b0b8ead63712bb8d39b5e4a7c9c971
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-08 13:37:28 +0100

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = []
+    arr = str.split("")
+    
+    // result = arr.reverse() can be used in place of this for loop
 
+    for(let i = 0; i < str.length; i++) {
+        char = arr.pop()
+        result.push(char)
+    }
+    return result.join("")
 };
 
 // Do not edit below this line
