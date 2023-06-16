86cd92c763be7a63b7b05a95a79d60007c65aec1
wes-ubuntu-desktop <wesleyshaw25@gmail.com>
2023-06-16 16:32:39 +0100

    Finished exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldestPerson, newPerson) => {
+        let newAge = getAge(newPerson)
+        let oldestAge = getAge(oldestPerson)
+        if (newAge > oldestAge) {
+            return newPerson
+        }
+        else {
+            return oldestPerson
+        }
+    })
 };
+
+function getAge(person) {
+    if(!person['yearOfDeath']) {
+        return (new Date().getFullYear() - person.yearOfBirth)
+    }
+    else {
+        return person.yearOfDeath - person.yearOfBirth
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
